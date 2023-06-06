5b867c59aabc2dd293cfdfee4d4760178885b9ca
emareaun <emareaun12345@gmail.com>
2023-06-05 21:43:04 -0400

    changed character types and added length

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-// Assignment code here
-
-// Get references to the #generate element
+// Get references to the #generate, #password, and #alert elements
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var alertBtn = document.querySelector("#alert");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-generateBtn.addEventListener("click", writePassword);
-
+// Generate password function
 function generatePassword() {
-    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-  
+  // Get the length of the password
+  var length = prompt("Enter the length of the password (8-128): ");
+
+  // Get the character types to include in the password
+  var charTypes = prompt("Enter the character types to include (lowercase, uppercase, numeric, special): ");
+
+  // Validate the input
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please enter a valid length for the password");
+    return;
+  }
+
+  var chars = "";
+  if (charTypes.includes("lowercase")) {
+    chars += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (charTypes.includes("uppercase")) {
+    chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (charTypes.includes("numeric")) {
+    chars += "0123456789";
+  }
+  if (charTypes.includes("special")) {
+    chars += "!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?";
+  }
+
+  // Generate a random password of the specified length
   var password = "";
-  for (var i = 0; i < 12; i++) {
-    
+  for (var i = 0; i < length; i++) {
     var charIndex = Math.floor(Math.random() * chars.length);
     var char = chars[charIndex];
 
     password += char;
   }
 
-  return password;
+  // Display the password in an alert
+  alert("Your password is: " + password);
+
+  // Write the password to the page
+  passwordText.value = password;
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
+// Add event listener to alert button
+alertBtn.addEventListener("click", function() {
+  alert("Your password is: " + passwordText.value);
+});
